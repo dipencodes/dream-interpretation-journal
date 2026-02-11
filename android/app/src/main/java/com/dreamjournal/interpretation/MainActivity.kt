93e9d6b5000fad49e5f144ee1fa4ce2b2b482441
Dipendra Pokharel <dipen@Dipendras-MacBook-Pro.local>
2026-02-10 21:01:46 -0600

    Renamed project

--- android/app/src/main/java/com/dreamjournal/interpretation/MainActivity.kt
+++ android/app/src/main/java/com/dreamjournal/interpretation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.aiastrologymobile
+package com.dreamjournal.interpretation
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "AiAstrologyMobile"
+  override fun getMainComponentName(): String = "DreamInterpretationJournal"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
--- android/app/src/main/java/com/dreamjournal/interpretation/MainApplication.kt
+++ android/app/src/main/java/com/dreamjournal/interpretation/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.aiastrologymobile
+package com.dreamjournal.interpretation
 
 import android.app.Application
 import com.facebook.react.PackageList
